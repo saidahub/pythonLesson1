edbb038d05e5e5d852250b933c2d69d3b2239325
saidahub <saidalovelife@163.com>
2021-02-09 18:05:03 +0800

    pytest插件学习fixture中的scope的学习

--- testting/test_calcultor_fixture.py
+++ testting/test_calcultor_fixture.py
@@ -17,7 +17,7 @@ def get_datas(name, type='int'):
 
 
 # 添加一个fixture 相当于setup teardown
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def get_instance():
     print("开始计算")
     calc: list = Calculator()
@@ -80,6 +80,7 @@ class TestCalc:
     #    @pytest.mark.parametrize("a, b, result", add_float_data[0], ids=add_float_data[1])
     def test_add_float(self, get_instance, get_calc_with_fixture_float):
         f = get_calc_with_fixture_float
+        print(f)
         # round()方法保留小数点后的位数
         assert f[2] == round(get_instance.add(f[0], f[1]), 2)
 
@@ -88,12 +89,14 @@ class TestCalc:
     @pytest.mark.div
     def test_div_int(self, get_instance, get_calc_with_fixture_div_int):
         div_int = get_calc_with_fixture_div_int
+        print(div_int)
         assert div_int[2] == get_instance.div(div_int[0], div_int[1])
 
     # 浮点数测试用例设计
     #    @pytest.mark.parametrize("a, b, result", div_float_data[0], ids=div_float_data[1])
     def test_div_float(self, get_instance, get_calc_with_fixture_div_float):
         div_float = get_calc_with_fixture_div_float
+        print(div_float)
         assert div_float[2] == round(get_instance.div(div_float[0], div_float[1]), 2)
 
     # 异常情况测试用例设计
@@ -101,6 +104,7 @@ class TestCalc:
     def test_div_abnormal(self, get_instance, get_calc_with_fixture_div_abnormal):
         with pytest.raises(ZeroDivisionError):
             div_abnormal = get_calc_with_fixture_div_abnormal
+            print(div_abnormal)
             assert get_calc_with_fixture_div_abnormal[2] == get_instance.div(get_calc_with_fixture_div_abnormal[0],
                                                                              get_calc_with_fixture_div_abnormal[1])
             print("除数不能为0")
